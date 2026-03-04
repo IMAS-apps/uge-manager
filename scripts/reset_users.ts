da1396248086f20a3d18ba2a57d671ccef7ddf68
TomeuKuma <bartomeumirallesllull@gmail.com>
2026-03-04 14:13:44 +0100

    Cleanup: Remove obsolete local server, scripts, and unused dependencies

--- scripts/reset_users.ts
+++
@@ -1,6 +0,0 @@
-import Database from 'better-sqlite3';
-const db = new Database('imas.db');
-console.log('Resetting users table...');
-db.prepare('DELETE FROM users').run();
-db.prepare("DELETE FROM sqlite_sequence WHERE name = 'users'").run();
-console.log('Done.');
